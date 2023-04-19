c2ff8bba8819069775abfb26f1d6a2b738ba0ec4
Shin레카닉 <sin3952@gmail.com>
2023-04-19 04:50:30 +0000

    20220995번 미디어소프트웨어학과 신상우 자바웹프로그래밍 7주차 수업 진행

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function over(obj) {
+	obj.src="img/OIP.png";
+}
+
+function out(obj) {
+	obj.src="img/Google_Logo_19981.png";
+}
--- js/search.js
+++ js/search.js
@@ -1,8 +1,31 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
+let F1="해병문학";
+let F2="빡대가리";
+let F3="기둥속";
 
-function search_message(){
-	alert("거,검색을 수,수행합니다."); 
+var search_array = []; // 빈 배열 – 전역 변수
+
+function search_message()
+{
 	let search_str = document.querySelector("#search_txt"); // 변수에 저장
-	document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
-	console.log(search_str.value); // 콘솔에 출력
+	if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+		alert("이 빠,빽대가리야! 거,검색어가, 비,비었잖아!");
+    }
+	else{
+		if(search_str.value===F1||search_str.value===F2||search_str.value===F3)
+			{
+				alert("너,너희들, 비, 비속어는 조심하라고.");
+			}
+		else
+		{
+			alert("거,검색을 수,수행합니다.");
+			search_array.push(search_str.value); // 배열에 검색어 추가
+			if(search_array.length>10)
+				{
+					search_array.shift();
+				}
+			let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+			document.querySelector("#form_main").submit();
+		}
+	}
 }
