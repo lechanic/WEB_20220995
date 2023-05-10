445b8bb84ae1ff318fd1c73046936d5c1e5b9d92
Shin레카닉 <sin3952@gmail.com>
2023-05-10 05:24:54 +0000

    자바웹 10주차 강의

--- js/login.js
+++ js/login.js
@@ -2,16 +2,44 @@ function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
-    
+    let check=document.querySelector("#idSaveCheck");
     form.action = "../index_login.html";
     form.method = "get";//비밀번호 및 이메일 주소 드러냄
 	//form.method = "post";//비밀번호 및 이메일 주소 숨김
-    
+	if(check.checked == true) { // 아이디 체크 o
+            alert("쿠키를 저장합니다.");
+            setCookie("id", id.value, 1); // 1일 저장
+            alert("쿠키 값 :" + id.value);
+        } 
+    else { // 아이디 체크 x
+            setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.");
     }else{
+		//login_check(id, password);
         form.submit();
     }
+}
+function login_check(idcheck, passwordcheck)
+{
+	if((/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/).test(idcheck))
+	{
+		/*if(passwordcheck.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$/))
+		{/^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/)
+			form.submit;
+		}
+		else
+		{
+			alert("빠,빡대가리야! 비밀번호 형식도 까,까먹었냐?");
+		}*/
+		form.submit();
+	}
+	else
+		{
+			alert("저,적어도 아,아이디 형식은 알고 이,있으라고.");
+		}
 }
 function logout(){
 	alert("로그, 삼진 아웃");
@@ -33,5 +61,26 @@ function get_id(){
 	    } // 2중 for문 끝
 }; // 함수 끝
 alert('환영합니다,'+getParameters('id') + '님.'); // 메시지 창 출력
+}
+function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+	document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";      
+    }
+
+function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "id") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return ;
 }
 
